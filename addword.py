428cdb173a612ea7dc3caaa50f506a8e6e49ecfb
Aaron Gonsalves <aarongonsalves@Aarons-MacBook-Pro.local>
2019-04-03 19:05:51 +0100

    Bracket 6 completed and code optimized

--- addword.py
+++ addword.py
@@ -16,6 +16,74 @@ def ReorderingWord(unorderedWord):
     lexicographicalOrderedWord = ''.join(sorted(unorderedWord))
     return lexicographicalOrderedWord
 
+def storingAnagramWord(getLexicographicalWord, self):
+    getUserName = users.get_current_user()
+    myuser_key = ndb.Key('MyUser', getUserName.user_id())
+    myuser = myuser_key.get()
+
+    anagramWordLetterLength = len(getLexicographicalWord)
+    lexicographicalOrderedAnagramWord = ReorderingWord(getLexicographicalWord)
+
+    keyName = getUserName.user_id()+lexicographicalOrderedAnagramWord
+
+    retriveAnagramkey = ndb.Key(MyAnagramDatabase, keyName)
+    retriveAnagramWord = retriveAnagramkey.get()
+
+    if retriveAnagramWord == None:
+
+        userSpecificAnagramCount = MyUser(id=getUserName.user_id(), anagramwordlistcount = myuser.anagramwordlistcount+1,
+                                              anagramuniquewordcount = myuser.anagramuniquewordcount+1,
+                                              username=getUserName.email())
+
+        storedatabase = MyAnagramDatabase(id=keyName, orderedword=lexicographicalOrderedAnagramWord,
+                                              lettercount = anagramWordLetterLength, useremailid = getUserName,
+                                              wordcount = 1)
+
+        storedatabase.anagramwordlist.append(getLexicographicalWord)
+
+        userSpecificAnagramCount.put()
+        storedatabase.put()
+
+        template_values = {
+            'success' : 'Word added successfully to the system'
+        }
+        template = JINJA_ENVIRONMENT.get_template('main.html')
+        self.response.write(template.render(template_values))
+
+    else:
+        counterWordExist = 0
+        for storedAnagramWord in retriveAnagramWord.anagramwordlist:
+            if storedAnagramWord == getLexicographicalWord:
+                counterWordExist = 1
+                break
+
+        if counterWordExist == 1:
+            template_values = {
+                'error' : 'Word already exist in the system'
+            }
+            template = JINJA_ENVIRONMENT.get_template('main.html')
+            self.response.write(template.render(template_values))
+
+        else:
+            userSpecificAnagramCount = MyUser(id=getUserName.user_id(),
+                                              anagramuniquewordcount = myuser.anagramuniquewordcount,
+                                              anagramwordlistcount = myuser.anagramwordlistcount+1,
+                                              username=getUserName.email())
+
+            retriveAnagramListLength = len(retriveAnagramWord.anagramwordlist)
+            retriveAnagramWord.wordcount = retriveAnagramListLength + 1
+
+            retriveAnagramWord.anagramwordlist.append(getLexicographicalWord)
+
+            userSpecificAnagramCount.put()
+            retriveAnagramWord.put()
+
+            template_values = {
+                'success' : 'Word added successfully to the system'
+            }
+            template = JINJA_ENVIRONMENT.get_template('main.html')
+            self.response.write(template.render(template_values))
+
 class AddAnagram(webapp2.RequestHandler):
     def get(self):
         self.response.headers['content-Type'] = 'text/html'
@@ -33,78 +101,21 @@ class AddAnagram(webapp2.RequestHandler):
         self.response.write(template.render(template_values))
 
     def post(self):
-
         self.response.headers['content-Type'] = 'text/html'
 
         if self.request.get('submit') == 'Submit':
-
-            getUserName = users.get_current_user()
-            myuser_key = ndb.Key('MyUser', getUserName.user_id())
-            myuser = myuser_key.get()
+            self.response.headers['content-Type'] = 'text/html'
 
             getLexicographicalWord = self.request.get('anagram_word').lower()
-            anagramWordLetterLength = len(getLexicographicalWord)
-            lexicographicalOrderedAnagramWord = ReorderingWord(getLexicographicalWord)
+            storingAnagramWord(getLexicographicalWord, self)
 
-            keyName = getUserName.user_id()+lexicographicalOrderedAnagramWord
-
-            retriveAnagramkey = ndb.Key(MyAnagramDatabase, keyName)
-            retriveAnagramWord = retriveAnagramkey.get()
-
-            if retriveAnagramWord == None:
-
-                userSpecificAnagramCount = MyUser(id=getUserName.user_id(), anagramwordlistcount = myuser.anagramwordlistcount+1,
-                                                  anagramuniquewordcount = myuser.anagramuniquewordcount+1,
-                                                  username=getUserName.email())
-
-                storedatabase = MyAnagramDatabase(id=keyName, orderedword=lexicographicalOrderedAnagramWord,
-                                                  lettercount = anagramWordLetterLength, useremailid = getUserName,
-                                                  wordcount = 1)
-
-                storedatabase.anagramwordlist.append(getLexicographicalWord)
-
-                userSpecificAnagramCount.put()
-                storedatabase.put()
-
-                template_values = {
-                    'success' : 'Word added successfully to the system'
-                }
-                template = JINJA_ENVIRONMENT.get_template('main.html')
-                self.response.write(template.render(template_values))
-
-            else:
-                counterWordExist = 0
-                for storedAnagramWord in retriveAnagramWord.anagramwordlist:
-                    if storedAnagramWord == getLexicographicalWord:
-                        counterWordExist = 1
-                        break
-
-                if counterWordExist == 1:
-                    template_values = {
-                        'error' : 'Word already exist in the system'
-                    }
-                    template = JINJA_ENVIRONMENT.get_template('main.html')
-                    self.response.write(template.render(template_values))
-
-                else:
-                    userSpecificAnagramCount = MyUser(id=getUserName.user_id(),
-                                                      anagramuniquewordcount = myuser.anagramuniquewordcount,
-                                                      anagramwordlistcount = myuser.anagramwordlistcount+1,
-                                                      username=getUserName.email())
-
-                    retriveAnagramListLength = len(retriveAnagramWord.anagramwordlist)
-                    retriveAnagramWord.wordcount = retriveAnagramListLength + 1
-
-                    retriveAnagramWord.anagramwordlist.append(getLexicographicalWord)
-                    
-                    userSpecificAnagramCount.put()
-                    retriveAnagramWord.put()
-
-                    template_values = {
-                        'success' : 'Word added successfully to the system'
-                    }
-                    template = JINJA_ENVIRONMENT.get_template('main.html')
-                    self.response.write(template.render(template_values))
+        if self.request.get('button') == 'Upload':
+            self.response.headers['content-Type'] = 'text/html'
+            readingTextFile = self.request.POST['file']
+            splitingTheWordInList = readingTextFile.value.split("\n")
+            for takingSingleWord in splitingTheWordInList:
+                if takingSingleWord.isalpha():
+                    storingAnagramWord(takingSingleWord, self)
 
         if self.request.get('button') == 'Back':
             self.redirect('/')
